2425ffb6f2984b08b2e89680a91ac37022e922b1
ibhaudur rahman <ibhaudurrahman1999@gmail.com>
2024-08-06 15:28:16 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import React from "react";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
